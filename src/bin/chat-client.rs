7b44dbdd6db9e1ecc633a9a8a51f556e7be196a0
Robert Peterson <me@robertp.me>
2023-11-05 15:33:15 -0800

    client wip

--- src/bin/chat-client.rs
+++ src/bin/chat-client.rs
@@ -2,20 +2,10 @@ use clap::Parser;
 use tokio::{io::stdin, net::TcpStream};
 use tokio_stream::StreamExt;
 use tokio_util::codec::{Framed, FramedRead, LinesCodec};
+use futures::SinkExt;
 
-#[derive(Parser)]
-#[command(author, version, long_about = None)]
-struct Cli {
-    address: String,
-}
-
-#[tokio::main]
-async fn main() -> anyhow::Result<()> {
-    // parse command line arguments
-    let cli = Cli::parse();
-
-    // connect to server
-    let tcp_stream = TcpStream::connect(cli.address).await?;
+async fn client(tcp_stream: TcpStream) -> anyhow::Result<()> {
+    // read/write to socket stream
     let mut server_frame = Framed::new(tcp_stream, LinesCodec::new_with_max_length(1024));
 
     // read stdin
@@ -24,17 +14,58 @@ async fn main() -> anyhow::Result<()> {
 
     // main client loop
     loop {
+        println!("loop");
         tokio::select! {
             stdin_result = stdin_frame.next() => match stdin_result {
-                None => panic!("reader next None"),
-                Some(Err(_)) => panic!("reader error"),
-                Some(Ok(input)) => println!("STDIN: {}", input),
+                None => {
+                    println!("STDIN stream closed.");
+                    return Ok(());
+                },
+                Some(Err(e)) => {
+                    println!("Unknown error with STDIN stream. {}", e);
+                    return Ok(());
+                },
+                Some(Ok(input)) => server_frame.send(input).await?,
             },
             stream_result = server_frame.next() => match stream_result {
-                None => panic!("server conn next None"),
-                Some(Err(_)) => panic!("server_conn error"),
-                Some(Ok(message)) => println!("STREAM: {}", message),
+                None => {
+                    println!("Server disconnected.");
+                    return Ok(());
+                },
+                Some(Err(e)) => {
+                    println!("Unknown error with server stream. {}", e);
+                    return Ok(());
+                },
+                Some(Ok(message)) => println!("{}", message),
             }
         }
     }
+}
+
+#[derive(Parser)]
+#[command(author, version, long_about = None)]
+struct Cli {
+    address: String,
+}
+
+// TODO: spawn two threads: 
+//  - one thread for server frame io. 
+//  - one frame for blocking stdin io.
+// ref: https://docs.rs/tokio/latest/tokio/io/struct.Stdin.html
+// 
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    // parse command line arguments
+    let cli = Cli::parse();
+
+    // connect to server
+    let tcp_stream = TcpStream::connect(cli.address).await?;
+
+    // run client app
+    client(tcp_stream).await?;
+
+    
+    println!("Exiting.");
+
+    Ok(())
 }
