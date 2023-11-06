b0d7fc2a222d9bbe3f994be2f328f5d569c45e6c
Robert Peterson <me@robertp.me>
2023-11-05 20:31:41 -0800

    working user client

--- src/bin/chat-client.rs
+++ src/bin/chat-client.rs
@@ -1,46 +1,9 @@
+use anyhow::anyhow;
 use clap::Parser;
-use tokio::{io::stdin, net::TcpStream};
-use tokio_stream::StreamExt;
-use tokio_util::codec::{Framed, FramedRead, LinesCodec};
 use futures::SinkExt;
-
-async fn client(tcp_stream: TcpStream) -> anyhow::Result<()> {
-    // read/write to socket stream
-    let mut server_frame = Framed::new(tcp_stream, LinesCodec::new_with_max_length(1024));
-
-    // read stdin
-    let stdin = stdin();
-    let mut stdin_frame = FramedRead::new(stdin, LinesCodec::new());
-
-    // main client loop
-    loop {
-        println!("loop");
-        tokio::select! {
-            stdin_result = stdin_frame.next() => match stdin_result {
-                None => {
-                    println!("STDIN stream closed.");
-                    return Ok(());
-                },
-                Some(Err(e)) => {
-                    println!("Unknown error with STDIN stream. {}", e);
-                    return Ok(());
-                },
-                Some(Ok(input)) => server_frame.send(input).await?,
-            },
-            stream_result = server_frame.next() => match stream_result {
-                None => {
-                    println!("Server disconnected.");
-                    return Ok(());
-                },
-                Some(Err(e)) => {
-                    println!("Unknown error with server stream. {}", e);
-                    return Ok(());
-                },
-                Some(Ok(message)) => println!("{}", message),
-            }
-        }
-    }
-}
+use tokio::{net::TcpStream, sync::mpsc::unbounded_channel};
+use tokio_stream::StreamExt;
+use tokio_util::codec::{Framed, LinesCodec};
 
 #[derive(Parser)]
 #[command(author, version, long_about = None)]
@@ -48,11 +11,6 @@ struct Cli {
     address: String,
 }
 
-// TODO: spawn two threads: 
-//  - one thread for server frame io. 
-//  - one frame for blocking stdin io.
-// ref: https://docs.rs/tokio/latest/tokio/io/struct.Stdin.html
-// 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     // parse command line arguments
@@ -61,11 +19,45 @@ async fn main() -> anyhow::Result<()> {
     // connect to server
     let tcp_stream = TcpStream::connect(cli.address).await?;
 
-    // run client app
-    client(tcp_stream).await?;
+    // server frame
+    let mut server_frame = Framed::new(tcp_stream, LinesCodec::new_with_max_length(1024));
 
-    
-    println!("Exiting.");
+    // io bridge
+    let (iosend, mut iorecv) = unbounded_channel();
+    std::thread::spawn(move || {
+        for line in std::io::stdin().lines() {
+            iosend.send(line).unwrap();
+        }
+    });
 
-    Ok(())
+    loop {
+        tokio::select! {
+            server_result = server_frame.next() => match server_result {
+                None => {
+                    println!("Server disconnected.");
+                    return Ok(());
+                },
+                Some(Err(e)) => {
+                    println!("Stream error: {}", e);
+                    return Err(anyhow!(e));
+                },
+                Some(Ok(message)) => {
+                    println!("{}", message);
+                }
+            },
+            io_result = iorecv.recv() => match io_result {
+                None => {
+                    println!("Client disconnected.");
+                    return Ok(());
+                },
+                Some(Err(e)) => {
+                    println!("IO error: {}", e);
+                    return Err(anyhow!(e));
+                },
+                Some(Ok(input)) => {
+                    server_frame.send(input).await?;
+                }
+            }
+        }
+    }
 }
